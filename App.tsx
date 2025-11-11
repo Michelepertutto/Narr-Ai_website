68fd929c9e8248daf85c131cb0f5cddc4e0e93c9
Michelepertutto <michelepertutto@gmail.com>
2025-11-11 18:32:59 +0100

    update: fuck off

--- App.tsx
+++ App.tsx
@@ -296,7 +296,7 @@ const App = () => {
               </div>
             </div>
             
-            <div className="relative rounded-3xl shadow-2xl overflow-hidden">
+            <div className="mobile-hero-container relative rounded-3xl shadow-2xl overflow-hidden">
               <video
                 src={`${import.meta.env.BASE_URL}video/video-ai-per-audiolibri.mp4`}
                 poster={`${import.meta.env.BASE_URL}Imgs/Poster-video-background.png`}
