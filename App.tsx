02f48cc77e8e6a90433103bf97f6e80673010abb
Michelepertutto <michelepertutto@gmail.com>
2025-11-13 10:34:47 +0100

    update:fix

--- App.tsx
+++ App.tsx
@@ -357,7 +357,7 @@ const App = () => {
                   paddingTop: '5px'
                 }}
               >
-                <div className="flex items-center gap-3 px-5 pt-3">
+                <div className="flex items-center gap-3 px-5 pt-3 mb-[10px]">
                   <div className="flex-1 flex items-center gap-2 bg-gray-300 rounded-xl px-3 py-2">
                     <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
